04c1e05078c952091666eef665dcb28a73bb40e6
Mt-Youya <dd257248@163.com>
2025-10-26 01:16:50 +0800

    fix: build fix

--- packages/ui/build.ts
+++ packages/ui/build.ts
@@ -1,12 +1,13 @@
-import { exec } from "node:child_process"
+import { execSync } from "node:child_process"
 import fs from "node:fs"
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 
-exec("vite build --mode production")
+execSync("vite build --mode production")
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+console.log("__dirname", __dirname)
 
 const cssRaw = fs.readFileSync(path.join(__dirname, "dist/style.css"), "utf8")
 
