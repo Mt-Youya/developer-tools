aebdf4f79615632a27a99bb7a89b85d44a8f82f7
Mt-Youya <dd257248@163.com>
2025-10-26 01:57:56 +0800

    fix: bugfix

--- packages/ui/build.js
+++ packages/ui/build.js
@@ -3,16 +3,16 @@ import fs from "node:fs"
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 
-execSync("vite build --mode production")
+execSync("vite build")
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-console.log("__dirname", __dirname)
+// console.log("__dirname", __dirname)
 
 const cssRaw = fs.readFileSync(path.join(__dirname, "dist/style.css"), "utf8")
-console.log("cssRaw")
+// console.log("cssRaw")
 
 const appPath = path.join(__dirname, "../../apps/dev-tools/src/style/global.css")
-console.log("appPath", appPath)
+// console.log("appPath", appPath)
 
 fs.writeFileSync(appPath, cssRaw)
