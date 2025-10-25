ffa2c5c0c5b089ede7e06b60e7a2cd39cb09b27a
Mt-Youya <dd257248@163.com>
2025-10-25 23:01:46 +0800

    style fix

--- apps/dev-tools/src/main.tsx
+++ apps/dev-tools/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import "@devtools/ui/style.css"
 import "./index.css"
 import "./utils/idleCallback"
 import App from "./App.tsx"
